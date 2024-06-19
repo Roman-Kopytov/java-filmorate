bee3af82aab973ffd7bef97a2001ff6f0b5e81fe
DanielDaze <tattoo255@yandex.ru>
2024-06-19 23:32:59 +0300

    tests fix

--- src/test/java/ru/yandex/practicum/filmorate/dao/film/JdbcFilmRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/film/JdbcFilmRepositoryTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.time.LocalDate;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,15 +25,15 @@ class JdbcFilmRepositoryTest {
     static Film getTestFilm(int filmId) {
         LinkedHashSet<Genre> genres1 = new LinkedHashSet<>(List.of(new Genre(1L, "Комедия"), new Genre(2L, "Драма")));
         Film film1 = new Film(1L, "TJjtJuGS8dUeAzm", "HZOE3ct3plkt3m4ip6dN4EMzqop93SdO5QdJD16uzdhDIgUaQl",
-                LocalDate.of(1962, 1, 31), 156L, genres1, new Mpa(3, "PG-13"));
+                LocalDate.of(1962, 1, 31), 156L, genres1, new Mpa(3, "PG-13"), (HashSet<Director>) Set.of(new Director(1L)));
 
         LinkedHashSet<Genre> genres2 = new LinkedHashSet<>(List.of(new Genre(3L, "Мультфильм")));
         Film film2 = new Film(2L, "8B8qgTBRtGKBdJN", "wrVuIIL79f228O2tecGsMdMVbltg1xKpz5qLz86LVHIOv9xJq1",
-                LocalDate.of(1962, 3, 6), 65L, genres2, new Mpa(4, "R"));
+                LocalDate.of(1962, 3, 6), 65L, genres2, new Mpa(4, "R"), (HashSet<Director>) Set.of(new Director(2L)));
 
         LinkedHashSet<Genre> genres3 = new LinkedHashSet<>(List.of(new Genre(4L, "Триллер")));
         Film film3 = new Film(3L, "uUk6L30WM5jNBHc", "cB22DWO2euD7py3KEnxpmqcBOh2sJZAOkHJP1pxgPlvbnuxEVW",
-                LocalDate.of(1997, 03, 04), 118L, genres3, new Mpa(1, "G"));
+                LocalDate.of(1997, 03, 04), 118L, genres3, new Mpa(1, "G"), (HashSet<Director>) Set.of(new Director(3L)));
         List<Film> films = new LinkedList<>(List.of(film1, film2, film3));
         return films.get(filmId - 1);
     }
@@ -42,7 +41,7 @@ class JdbcFilmRepositoryTest {
     static Film getFilmForUpdate() {
         LinkedHashSet<Genre> genres1 = new LinkedHashSet<>(List.of(new Genre(2L, "Драма")));
         Film film = new Film(1L, "TJjtJuGS8dUeAzm", "HZOE3ct3plkt3m4ip6dN4EMzqop93SdO5QdJD16uzdhDIgUaQl",
-                LocalDate.of(1962, 1, 31), 156L, genres1, new Mpa(3, "PG-13"));
+                LocalDate.of(1962, 1, 31), 156L, genres1, new Mpa(3, "PG-13"), (HashSet<Director>) Set.of(new Director(1L)));
         return film;
     }
 
