8e15195f23803dd5c64311acfd722838ceca629f
DanielDaze <tattoo255@yandex.ru>
2024-06-19 21:41:45 +0300

    before PR

--- src/main/java/ru/yandex/practicum/filmorate/dao/director/DirectorRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/director/DirectorRepository.java
@@ -1,19 +1,17 @@
 package ru.yandex.practicum.filmorate.dao.director;
 
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import ru.yandex.practicum.filmorate.exception.BadBodyRequestException;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import java.util.List;
 
 public interface DirectorRepository {
-    public List<Director> getAll();
+    List<Director> getAll();
 
-    public Director getById(long id);
+    Director getById(long id);
 
-    public Director create(Director director);
+    Director create(Director director);
 
-    public Director update(Director director);
+    Director update(Director director);
 
-    public void delete(long id);
+    void delete(long id);
 }
