0b4bf6ba8eda74d11f5e3279dcc614520fc398a1
andrey <perminand@yandex.ru>
2024-06-22 14:27:46 +0300

    feat: chenge name method mapToUserDto to mapToFilmDto

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
@@ -7,7 +7,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class FilmMapper {
-    public static FilmDto mapToUserDto(Film film) {
+    public static FilmDto mapToFilmDto(Film film) {
         FilmDto dto = new FilmDto();
         dto.setId(film.getId());
         dto.setName(film.getName());
