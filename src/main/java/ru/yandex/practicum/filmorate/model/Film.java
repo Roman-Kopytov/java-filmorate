76cf59143447d55c00a4fe58e24feb819dd67bf3
Georgii <moroz.georgijs@yandex.ru>
2024-06-22 23:18:58 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.Marker.Update;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.List;
 
 /**
  * Film.
