092dcaf414674b1fdb50b557b2fbc210a72d9f0b
andrey <perminand@yandex.ru>
2024-06-22 14:43:41 +0300

    feat: change User to Optional<User> method getById

--- src/main/java/ru/yandex/practicum/filmorate/dao/user/JdbcUserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/user/JdbcUserRepository.java
@@ -11,6 +11,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,9 +21,9 @@ public class JdbcUserRepository implements UserRepository {
     private final UserRowMapper userRowMapper;
 
     @Override
-    public User getById(long userId) {
-        return jdbcOperations.queryForObject("SELECT * FROM users WHERE user_id =:userId",
-                Map.of("userId", userId), userRowMapper);
+    public Optional<User> getById(long userId) {
+        return Optional.ofNullable(jdbcOperations.queryForObject("SELECT * FROM users WHERE user_id =:userId",
+                Map.of("userId", userId), userRowMapper));
     }
 
     @Override
