429541322db5efc81d1cff1cdd10097ae7f014f2
Perminand <145030012+Perminand@users.noreply.github.com>
2024-06-23 13:37:36 +0300

    Add most populars (#13)
    * feat: Добавлен вывод популярности по жанру и годам

--- src/main/java/ru/yandex/practicum/filmorate/dao/film/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/film/FilmRepository.java
@@ -23,7 +23,7 @@ public interface FilmRepository {
 
     void deleteLike(Film film, User user);
 
-    List<Film> getTopPopular(int count);
-
     List<Film> getSortedFilmsByDirector(long directorId, String sortBy);
+
+    List<Film> getTopPopular(int count, Long genreId, Integer year);
 }
